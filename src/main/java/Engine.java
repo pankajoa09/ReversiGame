650023114161719eeb10a143301924ee00e302c9
Pankaj Ahuja <pankaj.oa.09@gmail.com>
2017-09-25 01:18:40 +0700

    nearly died there

--- src/main/java/Engine.java
+++ src/main/java/Engine.java
@@ -10,8 +10,8 @@ public class Engine {
 
     public boolean isPermissible(Block block, String turn, Board board) {
         //If there are no victims then move and block is not empty then not valid
-        ArrayList<ArrayList<Block>> victims = getVictims(block,turn,board);
-        return (!victims.isEmpty()) && (block.getType().equals("Empty"));
+        ArrayList<ArrayList<Block>> listOfDisksToFlip = getAllDisksToFlip(block,turn,board);
+        return (!listOfDisksToFlip.isEmpty()) && (block.getType().equals("Empty"));
 
     }
 
@@ -20,54 +20,53 @@ public class Engine {
         Board board = new Board();
         board.createBoard();
         board.copyBoard(oldBoard);
-        ArrayList<ArrayList<Block>> victims = getVictims(block, turn, board);
-        board = executeVictims(turn, block,victims, board);
+        ArrayList<ArrayList<Block>> victims = getAllDisksToFlip(block, turn, board);
+        board = flipAllDisks(turn, block,victims, board);
 
         return board;
     }
 
 
-    public ArrayList<ArrayList<Block>> getVictims(Block block, String turn, final Board board) {
-        String toKill = "";
+    public ArrayList<ArrayList<Block>> getAllDisksToFlip(Block block, String turn, Board board) {
+        String toFlip = "";
         if (turn.equals("Black")) {
-            toKill = "White";
+            toFlip = "White";
         } else if (turn.equals("White")) {
-            toKill = "Black";
+            toFlip = "Black";
         }
-        ArrayList<ArrayList<Block>> groupOfFiringSquad = new ArrayList<ArrayList<Block>>();
+        ArrayList<ArrayList<Block>> bigListOfOpposers = new ArrayList<ArrayList<Block>>();
 
-        Block[] surr = seeSurroundingLand(block, board);
-        for (int i = 0; i < surr.length; i++) {
-            Block nextOne = surr[i];
+        Block[] surrounding = seeSurroundingBlocks(block, board);
+        for (int direction = 0; direction < surrounding.length; direction++) {
+            Block nextOne = surrounding[direction];
             // if a block is white
-            if (nextOne.getType().equals(toKill)) {
+            if (nextOne.getType().equals(toFlip)) {
                 // see whats further down that road and round em up
 
-
-                ArrayList<Block> firingSquad = roundUpTheNazis(block, i, toKill, turn, board);
+                ArrayList<Block> smallListOfOpposers = getDisksToFlip(block, direction, toFlip, turn, board);
                 //if the list came out empty means we couldnt kill em
 
-                if (!firingSquad.isEmpty()) {
-                    groupOfFiringSquad.add(firingSquad);
+                if (!smallListOfOpposers.isEmpty()) {
+                    bigListOfOpposers.add(smallListOfOpposers);
                 }
 
             }
 
         }
 
-        return groupOfFiringSquad;
+        return bigListOfOpposers;
     }
 
 
-    private Board executeVictims(String turn, Block block, ArrayList<ArrayList<Block>> groupOfFiringSquad,Board board) {
+    private Board flipAllDisks(String turn, Block block, ArrayList<ArrayList<Block>> bigListOfDisksToFlip,Board board) {
 
-        if (!groupOfFiringSquad.isEmpty()) {
-            for (int it = 0; it < groupOfFiringSquad.size(); it++) {
-                ArrayList<Block> firingSquad = groupOfFiringSquad.get(it);
+        if (!bigListOfDisksToFlip.isEmpty()) {
+            for (int it = 0; it < bigListOfDisksToFlip.size(); it++) {
+                ArrayList<Block> smallListOFDisksToFlip = bigListOfDisksToFlip.get(it);
 
                 //if at the end there was a black this list would have something in it.
                 //then kill em or convert em tbh
-                board = killTheNazis(firingSquad, turn,board);
+                board = flipDisks(smallListOFDisksToFlip, turn,board);
                 //then kill the new one too
                 block.setType(turn);
                 board.addBlock(block);
@@ -78,9 +77,9 @@ public class Engine {
         return board;
     }
 
-    private Board killTheNazis(ArrayList<Block> nazis,String turn,Board board){
-        for (int i = 0; i < nazis.size(); i++){
-            Block curr = nazis.get(i);
+    private Board flipDisks(ArrayList<Block> smallListOfDisksToFlip,String turn,Board board){
+        for (int i = 0; i < smallListOfDisksToFlip.size(); i++){
+            Block curr = smallListOfDisksToFlip.get(i);
             curr.setAll(curr.getI(),curr.getJ(),turn);
             board.addBlock(curr);
 
@@ -89,57 +88,56 @@ public class Engine {
         return board;
     }
 
-    private ArrayList<Block> roundUpTheNazis(Block firstNazi, int direction, String toKill, String turn, Board baard){
-        ArrayList<Block> naziJail = new ArrayList<Block>();
+    private ArrayList<Block> getDisksToFlip(Block firstNazi, int direction, String toFlip, String turn, Board board){
+        ArrayList<Block> listOfOpposers = new ArrayList<Block>();
 
-        int d = direction;
-        Block nextNazi;
-        nextNazi = seeSurroundingLand(firstNazi,baard)[d];
+        Block nextOpposer;
+        nextOpposer = seeSurroundingBlocks(firstNazi,board)[direction];
 
         while (true) {
 
-            if (nextNazi.getType().equals("OutBound") || nextNazi.getType().equals("Empty")){
-                naziJail.clear();
+            if (nextOpposer.getType().equals("OutBound") || nextOpposer.getType().equals("Empty")){
+                listOfOpposers.clear();
                 break;
             }
-            else if (nextNazi.getType().equals(turn)){
+            else if (nextOpposer.getType().equals(turn)){
                 break;
             }
 
-            else if (nextNazi.getType().equals(toKill)) {
-                naziJail.add(nextNazi);
+            else if (nextOpposer.getType().equals(toFlip)) {
+                listOfOpposers.add(nextOpposer);
             }
 
-            nextNazi = seeSurroundingLand(nextNazi,baard)[d];
+            nextOpposer = seeSurroundingBlocks(nextOpposer,board)[direction];
         }
 
-        return naziJail;
+        return listOfOpposers;
     }
 
 
-    private Block[] seeSurroundingLand(Block selected,Board baard){
+    private Block[] seeSurroundingBlocks(Block selected,Board board){
         int currI = selected.getI();
         int currJ = selected.getJ();
         Block[] surroundings = new Block[8];
 
         //its clockwise surroundings so the first one is north and the next is north east etc.
-        surroundings[0] = getBlock(currI-1,currJ+0,baard);
-        surroundings[1] = getBlock(currI-1,currJ+1,baard);
-        surroundings[2] = getBlock(currI+0,currJ+1,baard);
-        surroundings[3] = getBlock(currI+1,currJ+1,baard);
-        surroundings[4] = getBlock(currI+1,currJ+0,baard);
-        surroundings[5] = getBlock(currI+1,currJ-1,baard);
-        surroundings[6] = getBlock(currI+0,currJ-1,baard);
-        surroundings[7] = getBlock(currI-1,currJ-1,baard);
+        surroundings[0] = getBlock(currI-1,currJ+0,board);
+        surroundings[1] = getBlock(currI-1,currJ+1,board);
+        surroundings[2] = getBlock(currI+0,currJ+1,board);
+        surroundings[3] = getBlock(currI+1,currJ+1,board);
+        surroundings[4] = getBlock(currI+1,currJ+0,board);
+        surroundings[5] = getBlock(currI+1,currJ-1,board);
+        surroundings[6] = getBlock(currI+0,currJ-1,board);
+        surroundings[7] = getBlock(currI-1,currJ-1,board);
         return surroundings;
     }
 
 
-    private Block getBlock(int i,int j, Board baard){
+    private Block getBlock(int i,int j, Board board){
         Block newBlock = new Block();
-        if ((i>=0 && i<=baard.getSize()-1) && (j>=0 && j<=baard.getSize()-1)) {
+        if ((i>=0 && i<=board.getSize()-1) && (j>=0 && j<=board.getSize()-1)) {
             //we give a go
-            newBlock = baard.getBlockFromGrid(i,j);
+            newBlock = board.getBlockFromGrid(i,j);
         }
         else{
             newBlock.setAll(i,j,"OutBound");
